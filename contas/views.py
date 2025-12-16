33829d5af33cfcdbb0ad9f1e8e5d944716cf3d77
User <rogerioparacambi@gmail.com>
2025-12-16 10:00:14 -0300

    Tela de login

---
+++ contas/views.py
@@ -0,0 +1,22 @@
+# contas/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # redireciona para a página principal
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+    else:
+        form = LoginForm()
+    return render(request, 'contas/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
